b4ff793e08351fdeaec2fead7892bdac7ebc806f
Robin Ricard <ricard.robin@gmail.com>
2015-03-13 09:42:48 +0100

    Rename

--- src/ASTTokenNode.h
+++ src/ASTTokenNode.h
@@ -1,21 +1,21 @@
 // Copyright 2015 H4314
-#ifndef SRC_ASTTERMINALIDENTIFIERNODE_H_
-#define SRC_ASTTERMINALIDENTIFIERNODE_H_
+#ifndef SRC_ASTTOKENNODE_H_
+#define SRC_ASTTOKENNODE_H_
 
 #include <string>
 #include "ASTNode.h"
 
 using std::string;
 
-class ASTTerminalIdentifierNode : public ASTNode {
+class ASTTokenNode : public ASTNode {
  private:
   string matchedRegex;
   string symbol;
  public:
-  ASTTerminalIdentifierNode(string matchedRegex, string symbol);
+  ASTTokenNode(string matchedRegex, string symbol);
   bool analyze(/* TODO: define the data-struct to pass */);
   void exec(/* TODO: define the data-struct to pass */);
   string getMatchedRegex();
   string getSymbol();
 };
-#endif  // SRC_ASTTERMINALIDENTIFIERNODE_H_
+#endif  // SRC_ASTTOKENNODE_H_
