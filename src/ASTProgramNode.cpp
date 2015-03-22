488b473665d876c67151b59b79b366171691cf3f
Robin Ricard <ricard.robin@gmail.com>
2015-03-22 18:23:31 +0100

    Finalize to program node

---
+++ src/ASTProgramNode.cpp
@@ -0,0 +1,31 @@
+//
+//  ASTProgramNode.cpp
+//  lut-lang
+//
+//  Created by Robin Ricard on 22/03/15.
+//  Copyright (c) 2015 INSA Lyon. All rights reserved.
+//
+
+#include "ASTProgramNode.h"
+
+ASTProgramNode::ASTProgramNode(ASTDeclarationBlockNode* declarations,
+                               ASTInstructionBlockNode* instructions) {
+  this->declarations = declarations;
+  this->instructions = instructions;
+}
+
+ASTDeclarationBlockNode* ASTProgramNode::getDeclarations() {
+  return this->declarations;
+}
+
+ASTInstructionBlockNode* ASTProgramNode::getInstructions() {
+  return this->instructions;
+}
+
+bool ASTProgramNode::analyze() {
+  return this->declarations->analyze() && this->instructions->analyze();
+}
+void ASTProgramNode::exec() {
+  this->declarations->exec();
+  this->instructions->exec();
+}
