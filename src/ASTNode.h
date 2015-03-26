297a8656f60f3d83b09135db3e1b8781758e2bad
mkitane <mehdikitane@gmail.com>
2015-03-26 14:20:48 +0100

    tokenNode

--- src/ASTNode.h
+++ src/ASTNode.h
@@ -2,22 +2,31 @@
 #ifndef SRC_ASTNODE_H_
 #define SRC_ASTNODE_H_
 
+#include <iostream>
+#include "TokenType.h"
+using std::string;
 /**
  * @class
  * Defines a base AST Node
  */
 class ASTNode {
+ private:
+  TokenType type;
+  string value;
  public:
   /**
    * Analyzes statically the validity of the program
    * @todo define the data-struct to pass
    */
   virtual bool analyze() = 0;
-
+  
   /**
    * Executes the program
    * @todo define the data-struct to pass
    */
   virtual void exec() = 0;
+  
+  TokenType getTokenType();
+  string getValue();
 };
 #endif  // SRC_ASTNODE_H_
