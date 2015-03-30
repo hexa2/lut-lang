a519e045f98223980c838c1b10e15a7b2b9b619b
mkitane <mehdikitane@gmail.com>
2015-03-30 08:59:17 +0200

    EnumDecl

--- src/ASTEnumDeclNode.cpp
+++ src/ASTEnumDeclNode.cpp
@@ -3,9 +3,12 @@
 #include "ASTEnumDeclNode.h"
 
 #include <iostream>
-
+#include <tuple>
+#include <utility>
+#include <string>
 using std::cout;
 using std::endl;
+using std::pair;
 
 ASTEnumDeclNode::ASTEnumDeclNode(ASTTokenNode* identifier,
                                  ASTEnumDeclNode* prev,
@@ -23,10 +26,24 @@ ASTEnumDeclNode* ASTEnumDeclNode::getPrev() {
 }
 
 bool ASTEnumDeclNode::analyze(analyze_table* table) {
+  if (this->prev != NULL && !this->prev->analyze(table)) {
+    return false;
+  }
+
+  table->insert(pair<string, tuple<bool, bool>>(this->identifier->getValue(),
+                                                std::make_tuple(false, false)));
+
   return true;
 }
 
 int64_t ASTEnumDeclNode::exec(exec_table* table) {
+  if (this->prev != NULL) {
+    this->prev->exec(table);
+  }
+
+  table->insert(pair<string, tuple<bool, bool>>(this->identifier->getValue(),
+                                                std::make_tuple(0, false)));
+
   return 0;
 }
 
